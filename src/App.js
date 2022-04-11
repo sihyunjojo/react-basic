292102188fc3aebba67786430752b7e895c5aefa
sihyunjojo <si4018@naver.com>
2022-04-11 15:45:44 +0900

    uesEffect complete

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import {useState, useEffect} from "react";
 
 function App() {
     const [counter,setValue] = useState(0);
+    const [keyword, setKeyword] = useState(""); 
     const onClick = () => setValue((prev) => prev + 1);
-    console.log("i run all the time");
+    const onChange = (event) => setKeyword(event.target.value);
+
     useEffect(() => {
-        console.log("Call The API....");
-    },[]);
+        console.log("i run only once");
+    }, []);
+    useEffect(() => {
+        console.log("i run when 'keyword' change.");
+    }, [keyword]);
+    useEffect(() => {
+        console.log("i run when 'counter' change.");
+    }, [counter]);
+
     return(
         <div>
+            <input 
+            value={keyword}
+            onChange = {onChange}
+            type="text"
+            placeholder="Search here..."
+            />
             <h1>{counter}</h1>
             <button onClick = {onClick}>click me</button>
        </div>
