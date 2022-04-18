133d3e8433469f77337ad1e8f9add9063a60566f
sihyunjojo <si4018@naver.com>
2022-04-18 22:18:19 +0900

    월요일 미션 클리어

---
+++ 5.4-src/App5.4.js
@@ -0,0 +1,33 @@
+import { func } from "prop-types";
+import {useState, useEffect} from "react";
+
+
+function Hello(){
+    useEffect(() => {
+        console.log("hi :)")
+        return () => console.log("bye :(")
+    },[]);
+    useEffect(function () {
+        console.log("hi :)");
+        return function () {
+            console.log("bye :(");
+        }
+    },[]);
+
+    useEffect(hiFn, []);
+    return <h1>Hello</h1>;
+}
+
+function App(){
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
+    return (
+    <div>
+        {showing ? <Hello /> : null}
+        <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+    )
+}
+
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import { func } from "prop-types";
-import {useState, useEffect} from "react";
+import { useState } from "react";
 
-
-function Hello(){
-    useEffect(() => {
-        console.log("hi :)")
-        return () => console.log("bye:(")
-    },[]);
-    useEffect(function () {
-        console.log("hi :)");
-        return function () {
-            console.log("bye :(");
-        }
-    },[]);
-
-    useEffect(hiFn, []);
-    return <h1>Hello</h1>;
-}
-
-function App(){
-    const [showing, setShowing] = useState(false);
-    const onClick = () => setShowing((prev) => !prev);
-    return (
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefult();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  return (
     <div>
-        {showing ? <Hello /> : null}
-        <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button> Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
-    )
+  );
 }
-
 
 export default App;
